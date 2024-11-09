7ec9ac5b20c608cbb984c36f641c52150c1bc46d
JasFerz001 <164256098+JasFerz001@users.noreply.github.com>
2024-11-08 23:04:06 -0600

    miau

---
+++ src/Controladores/Iniciar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controladores;
+
+import Vistas.Dashboard;
+
+/**
+ *
+ * @author kevin
+ */
+public class Iniciar {
+
+    public static void main(String[] args) {
+        System.out.println("holi");        
+        Dashboard dashboard = new Dashboard();
+        dashboard.setVisible(true);
+    }
+}
