2f5bf5997a021f29b5fb3b0c9b227b1d52af06ed
Lowis <aa22088@ues.edu.sv>
2024-11-16 14:34:17 -0600

    Balance General Finalizado

--- src/Controladores/ControladorBalanceGeneral.java
+++ src/Controladores/ControladorBalanceGeneral.java
@@ -46,11 +46,11 @@ public class ControladorBalanceGeneral {
         dtm2.addColumn("Total:");
         vista.tabla_pasivos.setModel(dtm2);
 
-       listaActivos=dao.CargarBalanceGeneral(activo);
-       lisAcNocorrientes=dao.CargarBalanceGeneral(aN);
-       listaPasivos=dao.CargarBalanceGeneral(pasivo);;  
-       listaPasivosNoCorrientes=dao.CargarBalanceGeneral(pN);
-       listaPatrimonio=dao.CargarBalanceGeneral(patrimonio);
+       listaActivos=dao.CargarBalanceGeneralActivos(activo);
+       lisAcNocorrientes=dao.CargarBalanceGeneralActivos(aN);
+       listaPasivos=dao.CargarBalanceGeneralPasivos(pasivo);;  
+       listaPasivosNoCorrientes=dao.CargarBalanceGeneralPasivos(pN);
+       listaPatrimonio=dao.CargarBalanceGeneralPasivos(patrimonio);
        
        agregarATabla();
     }
@@ -59,52 +59,51 @@ public class ControladorBalanceGeneral {
         int aux = 0;
         
         //CONSULTAR EL TOTAL DE ACTIVOS CORRIENTES
-//        float total_AC = dao.GetTotal(activo);
+        float total_AC = dao.GetTotalCuentasActivo(activo);
         //CONSULTAR CADA CUENTA DE ACTIVOS CORRIENTES
-        dtm.addRow(new Object[]{"Activo Corriente", " ", "Agregar total"});
+        dtm.addRow(new Object[]{"Activo Corriente", " ", total_AC});
         for (int i = 0; i < listaActivos.size(); i++) {
             dtm.addRow(new Object[]{listaActivos.get(i).getCuenta(), listaActivos.get(i).getMonto(), " "});
         }
         
         //CONSULTAR EL TOTAL DE ACTIVOS NO CORRIENTES
-//        float total_ANC = dao.GetTotal(aN);
+        float total_ANC = dao.GetTotalCuentasActivo(aN);
         //CONSULTAR CADA CUENTA DE ACTIVOS NO CORRIENTES
-        dtm.addRow(new Object[]{"Activo No Corriente", " ", "Agregar total"});
+        dtm.addRow(new Object[]{"Activo No Corriente", " ", total_ANC});
         for (int i = 0; i < lisAcNocorrientes.size(); i++) {
             dtm.addRow(new Object[]{lisAcNocorrientes.get(i).getCuenta(), lisAcNocorrientes.get(i).getMonto(), " "});
         }
 
         //CONSULTAR EL TOTAL DE PASIVOS CORRIENTES
-//        float total_PC = dao.GetTotal(pasivo);
+        float total_PC = dao.GetTotalCuentasPasivo(pasivo);
         //CONSULTAR CADA CUENTA DE PASIVOS CORRIENTES
-        dtm2.addRow(new Object[]{"Pasivo Corriente", " ", "Agregar total"});
+        dtm2.addRow(new Object[]{"Pasivo Corriente", " ", total_PC});
         for (int i = 0; i < listaPasivos.size(); i++) {
             dtm2.addRow(new Object[]{listaPasivos.get(i).getCuenta(), listaPasivos.get(i).getMonto(), " "});
         }
         
         //CONSULTAR EL TOTAL DE PASIVOS NO CORRIENTES
-//        float total_PNC = dao.GetTotal(pN);
+        float total_PNC = dao.GetTotalCuentasPasivo(pN);
         //CONSULTAR CADA CUENTA DE PASIVOS CORRIENTES
-        dtm2.addRow(new Object[]{"Pasivo No Corriente", " ", "Agregar total"});
+        dtm2.addRow(new Object[]{"Pasivo No Corriente", " ", total_PNC});
         for (int i = 0; i < listaPasivosNoCorrientes.size(); i++) {
             dtm2.addRow(new Object[]{listaPasivosNoCorrientes.get(i).getCuenta(), listaPasivosNoCorrientes.get(i).getMonto(), " "});
         }
         
         //CONSULTAR EL TOTAL DE CAPITAL
-//        float total_CAP = dao.GetTotal(patrimonio);
+        float total_CAP = dao.GetTotalCuentasPasivo(patrimonio);
         //CONSULTAR CADA CUENTA DE TOTAL DE CAPITAL
-        dtm2.addRow(new Object[]{"Patrimonio", " ", "Agregar total"});
+        dtm2.addRow(new Object[]{"Patrimonio", " ", total_CAP});
         if (!listaPatrimonio.isEmpty()) {
             for (int i = 0; i < listaPatrimonio.size(); i++) {
                 dtm2.addRow(new Object[]{listaPatrimonio.get(i).getCuenta(), listaPatrimonio.get(i).getMonto(), " "});
             }
         }
         
+        float total_activos = dao.GetTotalActivos();
+        float total_pasivos = dao.GetTotalPasivos();
         
-//        float total_activos = dao.GetTotalActivos();
-//        float total_pasivos = dao.GetTotalPasivos();
-//        
-//        vista.totalActivos.setText(Float.toString(total_activos));
-//        vista.totalPasivos.setText(Float.toString(total_pasivos));
+        vista.totalActivos.setText(Float.toString(total_activos));
+        vista.totalPasivos.setText(Float.toString(total_pasivos));
     }
 }
--- src/daos/DaoBalanceGeneral.java
+++ src/daos/DaoBalanceGeneral.java
@@ -19,7 +19,7 @@ public class DaoBalanceGeneral {
     private PreparedStatement ps;
     private Connection accesoDB;
 
-    private static final String CONSULTAR_BALANZA = "SELECT \n"
+    private static final String CONSULTAR_BALANZA_ACTIVOS = "SELECT \n"
             + "s.cod_subcuenta AS codigo, \n"
             + "s.nombre AS nombre_subcuenta, \n"
             + "COALESCE(SUM(CASE \n"
@@ -34,29 +34,97 @@ public class DaoBalanceGeneral {
             + "GROUP BY s.cod_subcuenta, s.nombre\n"
             + "ORDER BY s.cod_subcuenta;";
 
-    private static final String CONSULTAR_TOTAL = "SELECT CAST(SUM(librodiario.monto) AS NUMERIC) AS total\n"
-            + "		 FROM librodiario, catalogo\n"
-            + "		 WHERE librodiario.codigo = catalogo.codigo\n"
-            + "		 AND catalogo.cuentatipo = ? ";
+    private static final String CONSULTAR_BALANZA_PASIVOS = "SELECT \n"
+            + "s.cod_subcuenta AS codigo, \n"
+            + "s.nombre AS nombre_subcuenta, \n"
+            + "COALESCE(SUM(CASE \n"
+            + "WHEN ld.transaccion = 'Debe' THEN -ld.monto::NUMERIC \n"
+            + "WHEN ld.transaccion = 'Haber' THEN ld.monto::NUMERIC \n"
+            + "ELSE 0 END), 0) AS saldo\n"
+            + "FROM subcuentas s \n"
+            + "JOIN cuentas_mayor cm ON s.cod_mayor = cm.cod_mayor\n"
+            + "JOIN cuentas_principales cp ON cm.cod_principal = cp.cod_principal\n"
+            + "LEFT JOIN libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta\n"
+            + "WHERE cp.nombre = ?\n"
+            + "GROUP BY s.cod_subcuenta, s.nombre\n"
+            + "ORDER BY s.cod_subcuenta;";
 
-    private static final String CONSULTAR_TOTAL_ACTIVOS = " SELECT CAST(SUM(librodiario.monto) AS NUMERIC) AS total\n"
-            + "		 FROM librodiario, catalogo\n"
-            + "		 WHERE librodiario.codigo = catalogo.codigo\n"
-            + "		 AND catalogo.cuentatipo < 3;";
+    private static final String CONSULTAR_TOTAL_CUENTAS_ACTIVO = "SELECT \n"
+            + "    COALESCE(SUM(CASE \n"
+            + "        WHEN ld.transaccion = 'Debe' THEN ld.monto::NUMERIC \n"
+            + "        WHEN ld.transaccion = 'Haber' THEN -ld.monto::NUMERIC \n"
+            + "        ELSE 0 \n"
+            + "    END), 0) AS total\n"
+            + "FROM \n"
+            + "    subcuentas s\n"
+            + "JOIN \n"
+            + "    cuentas_mayor cm ON s.cod_mayor = cm.cod_mayor\n"
+            + "JOIN \n"
+            + "    cuentas_principales cp ON cm.cod_principal = cp.cod_principal\n"
+            + "LEFT JOIN \n"
+            + "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta\n"
+            + "WHERE \n"
+            + "    cp.nombre = ?;";
 
-    private static final String CONSULTAR_TOTAL_PASIVOS = "SELECT CAST(SUM(librodiario.monto) AS NUMERIC) AS total\n"
-            + "		 FROM librodiario, catalogo\n"
-            + "		 WHERE librodiario.codigo = catalogo.codigo\n"
-            + "		 AND catalogo.cuentatipo >= 3;";
+    private static final String CONSULTAR_TOTAL_CUENTAS_PASIVO = "SELECT \n"
+            + "    COALESCE(SUM(CASE \n"
+            + "        WHEN ld.transaccion = 'Debe' THEN -ld.monto::NUMERIC \n"
+            + "        WHEN ld.transaccion = 'Haber' THEN ld.monto::NUMERIC \n"
+            + "        ELSE 0 \n"
+            + "    END), 0) AS total\n"
+            + "FROM \n"
+            + "    subcuentas s\n"
+            + "JOIN \n"
+            + "    cuentas_mayor cm ON s.cod_mayor = cm.cod_mayor\n"
+            + "JOIN \n"
+            + "    cuentas_principales cp ON cm.cod_principal = cp.cod_principal\n"
+            + "LEFT JOIN \n"
+            + "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta\n"
+            + "WHERE \n"
+            + "    cp.nombre = ?;";
 
-    public ArrayList<BalanceGeneral> CargarBalanceGeneral(String tipo) {
+    private static final String CONSULTAR_TOTAL_ACTIVOS = "SELECT \n"
+            + "    COALESCE(SUM(CASE \n"
+            + "        WHEN ld.transaccion = 'Debe' THEN ld.monto::NUMERIC \n"
+            + "        WHEN ld.transaccion = 'Haber' THEN -ld.monto::NUMERIC \n"
+            + "        ELSE 0 \n"
+            + "    END), 0) AS total\n"
+            + "FROM \n"
+            + "    subcuentas s\n"
+            + "JOIN \n"
+            + "    cuentas_mayor cm ON s.cod_mayor = cm.cod_mayor\n"
+            + "JOIN \n"
+            + "    cuentas_principales cp ON cm.cod_principal = cp.cod_principal\n"
+            + "LEFT JOIN \n"
+            + "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta\n"
+            + "WHERE \n"
+            + "    cp.nombre IN ('Activo Corriente', 'Activo No Corriente');";
+
+    private static final String CONSULTAR_TOTAL_PASIVOS = "SELECT \n"
+            + "    COALESCE(SUM(CASE \n"
+            + "        WHEN ld.transaccion = 'Debe' THEN -ld.monto::NUMERIC \n"
+            + "        WHEN ld.transaccion = 'Haber' THEN ld.monto::NUMERIC \n"
+            + "        ELSE 0 \n"
+            + "    END), 0) AS total\n"
+            + "FROM \n"
+            + "    subcuentas s\n"
+            + "JOIN \n"
+            + "    cuentas_mayor cm ON s.cod_mayor = cm.cod_mayor\n"
+            + "JOIN \n"
+            + "    cuentas_principales cp ON cm.cod_principal = cp.cod_principal\n"
+            + "LEFT JOIN \n"
+            + "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta\n"
+            + "WHERE \n"
+            + "    cp.nombre IN ('Pasivo Corriente', 'Pasivo No Corriente', 'Capital Contable', 'Ingresos', 'Gastos');";
+
+    public ArrayList<BalanceGeneral> CargarBalanceGeneralActivos(String tipo) {
 
         this.listaBalanceGeneral = new ArrayList();
 
         try {
 
             this.accesoDB = this.conexion.getConexion();
-            this.ps = this.accesoDB.prepareStatement(CONSULTAR_BALANZA);
+            this.ps = this.accesoDB.prepareStatement(CONSULTAR_BALANZA_ACTIVOS);
             this.ps.setString(1, tipo);
             this.rs = ps.executeQuery();
 
@@ -76,14 +144,63 @@ public class DaoBalanceGeneral {
         return this.listaBalanceGeneral;
     }
 
-    public float GetTotal(int tipo) {
+    public ArrayList<BalanceGeneral> CargarBalanceGeneralPasivos(String tipo) {
+
+        this.listaBalanceGeneral = new ArrayList();
+
+        try {
+
+            this.accesoDB = this.conexion.getConexion();
+            this.ps = this.accesoDB.prepareStatement(CONSULTAR_BALANZA_PASIVOS);
+            this.ps.setString(1, tipo);
+            this.rs = ps.executeQuery();
+
+            BalanceGeneral obj = null;
+            while (this.rs.next()) {
+                obj = new BalanceGeneral();
+                obj.setCodigo(rs.getString("codigo"));
+                obj.setCuenta(rs.getString("nombre_subcuenta"));
+                obj.setMonto(Float.toString(rs.getFloat("saldo")));
+                this.listaBalanceGeneral.add(obj);
+            }
+            this.conexion.cerrarConexiones();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.listaBalanceGeneral;
+    }
+
+    public float GetTotalCuentasActivo(String tipo) {
         float total = 0;
 
         try {
 
             this.accesoDB = this.conexion.getConexion();
-            this.ps = this.accesoDB.prepareStatement(CONSULTAR_TOTAL);
-            this.ps.setInt(1, tipo);
+            this.ps = this.accesoDB.prepareStatement(CONSULTAR_TOTAL_CUENTAS_ACTIVO);
+            this.ps.setString(1, tipo);
+            this.rs = ps.executeQuery();
+
+            while (this.rs.next()) {
+                total = rs.getFloat("total");
+            }
+            this.conexion.cerrarConexiones();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return total;
+
+    }
+
+    public float GetTotalCuentasPasivo(String tipo) {
+        float total = 0;
+
+        try {
+
+            this.accesoDB = this.conexion.getConexion();
+            this.ps = this.accesoDB.prepareStatement(CONSULTAR_TOTAL_CUENTAS_PASIVO);
+            this.ps.setString(1, tipo);
             this.rs = ps.executeQuery();
 
             while (this.rs.next()) {
