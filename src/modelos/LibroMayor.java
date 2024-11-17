82741f1d92b4dc4417469557ad54812fd2dccc1f
99levi99G <163485848+99levi99G@users.noreply.github.com>
2024-11-17 10:27:08 -0600

    cambio1
    modeloMayor,vistamedia,controladorvacio

---
+++ src/modelos/LibroMayor.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Mac
+ */
+public class LibroMayor {
+    
+    private String codigo;
+    private String nombre;
+    private String concepto;
+    
+    private Double debe;
+    private Double haber;
+    private Double saldo;
+
+    public LibroMayor() {
+    }
+
+    public LibroMayor(String codigo) {
+        this.codigo = codigo;
+    }
+
+    
+    
+    public LibroMayor(String codigo, String nombre, String concepto, Double debe, Double haber, Double saldo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.concepto = concepto;
+        this.debe = debe;
+        this.haber = haber;
+        this.saldo = saldo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Double getDebe() {
+        return debe;
+    }
+
+    public void setDebe(Double debe) {
+        this.debe = debe;
+    }
+
+    public Double getHaber() {
+        return haber;
+    }
+
+    public void setHaber(Double haber) {
+        this.haber = haber;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+   
+    
+}
