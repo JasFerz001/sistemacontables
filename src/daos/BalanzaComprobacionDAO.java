47eeef9c09526954b7d43ff248e338b556ce9cad
JesusMayora <vm22024@ues.edu.sv>
2024-11-18 20:57:09 -0600

    reparacion query

--- src/daos/BalanzaComprobacionDAO.java
+++ src/daos/BalanzaComprobacionDAO.java
@@ -10,24 +10,27 @@ import modelos.BalanzaComprobacion;
 public class BalanzaComprobacionDAO {
 
    
-    private static final String QUERY_BALANZA = 
-        "SELECT " +
-        "    s.cod_subcuenta AS codigo, " +
-        "    s.nombre AS cuenta, " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) AS debe, " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) AS haber, " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) - " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) AS saldodeudor, " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) - " +
-        "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) AS saldoacreedor " +
-        "FROM " +
-        "    subcuentas s " +
-        "LEFT JOIN " +
-        "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta " +
-        "GROUP BY " +
-        "    s.cod_subcuenta, s.nombre " +
-        "ORDER BY " +
-        "    s.cod_subcuenta";
+    private static final String QUERY_BALANZA
+            = "SELECT "
+            + "    cm.cod_mayor AS codigo_mayor, "
+            + "    cm.nombre AS cuenta_mayor, "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) AS debe, "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) AS haber, "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) - "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) AS saldodeudor, "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'HABER' THEN ld.monto ELSE 0 END), 0) - "
+            + "    COALESCE(SUM(CASE WHEN ld.transaccion = 'DEBE' THEN ld.monto ELSE 0 END), 0) AS saldoacreedor "
+            + "FROM "
+            + "    cuentas_mayor cm "
+            + "LEFT JOIN "
+            + "    subcuentas s ON cm.cod_mayor = s.cod_mayor "
+            + "LEFT JOIN "
+            + "    libro_diario ld ON s.cod_subcuenta = ld.cod_subcuenta "
+            + "GROUP BY "
+            + "    cm.cod_mayor, cm.nombre "
+            + "ORDER BY "
+            + "    cm.cod_mayor;";
+
 
     
     public List<BalanzaComprobacion> obtenerBalanzaComprobacion() {
