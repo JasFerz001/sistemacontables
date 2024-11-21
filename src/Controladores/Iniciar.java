85c3021c743d689e97a75d638811d43a2e596009
JasFerz001 <164256098+JasFerz001@users.noreply.github.com>
2024-11-21 15:17:55 -0600

    ok

--- src/Controladores/Iniciar.java
+++ src/Controladores/Iniciar.java
@@ -2,11 +2,8 @@ package controladores;
 
 import java.awt.Frame;
 import javax.swing.JFrame;
-<<<<<<< HEAD
 import vistas.AgregarCatalogo;
 import vistas.Catalogos;
-=======
->>>>>>> main
 import vistas.Dashboard;
 
 /**
@@ -16,11 +13,9 @@ import vistas.Dashboard;
 public class Iniciar {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
- 
-=======
+
         Dashboard principal = new Dashboard();
         principal.setVisible(true);
->>>>>>> main
+
     }
 }
