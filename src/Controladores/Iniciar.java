541fefedbe9a3b42d637452f716690d03370874e
JasFerz001 <164256098+JasFerz001@users.noreply.github.com>
2024-12-04 23:02:18 -0600

    a

--- src/Controladores/Iniciar.java
+++ src/Controladores/Iniciar.java
@@ -1,17 +1,7 @@
 package controladores;
 
-import Controladores.ControladorLibroMayor;
-import Vistas.VistaMayor;
-import javax.swing.JFrame;
-
-import vistas.Dashboard;
-
 import Controladores.ControladorLogin;
 import Vistas.Login;
-
-
-
-
 
 /**
  *
