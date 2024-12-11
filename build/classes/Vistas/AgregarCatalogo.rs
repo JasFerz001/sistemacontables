0d3cace35306405aaf995e909958f917b38c43fe
KevSant10 <santoskevin439@gmail.com>
2024-12-10 20:42:41 -0600

    Revert "."
    This reverts commit 4d7ee9aca3c3f931f9d3bff71822c59e2371b2c2.

---
+++ build/classes/Vistas/AgregarCatalogo.rs
@@ -0,0 +1,8 @@
+vistas.AgregarCatalogo$5
+vistas.AgregarCatalogo
+vistas.AgregarCatalogo$6
+vistas.AgregarCatalogo$3
+vistas.AgregarCatalogo$4
+vistas.AgregarCatalogo$1
+vistas.AgregarCatalogo$2
+vistas.AgregarCatalogo$7
